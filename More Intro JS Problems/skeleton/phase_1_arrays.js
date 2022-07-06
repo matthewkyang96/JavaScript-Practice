3e921c3251a35ef4266f00f54d9d2f036a08f06c
Matthew <matthewkyang96@gmail.com>
2022-07-05 21:18:51 -0700

    monkey patch Array class

--- More Intro JS Problems/skeleton/phase_1_arrays.js
+++ More Intro JS Problems/skeleton/phase_1_arrays.js
@@ -1,1 +1,38 @@
-j
+//Monkey-patching Array Class
+
+Array.prototype.uniq = function (){
+    let uniqueArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+        if (!uniqueArray.includes(this[i])) {
+            uniqueArray.push(this[i]);
+        }
+    }
+    return uniqueArray;
+};
+
+Array.prototype.twoSum = function (){
+    let pairsIndex = [];
+    for (let i = 0; i < this.length - 1; i ++){
+        for (let j = i + 1; j < this.length; j ++){
+            if (this[i]+this[j] === 0){
+                pairsIndex.push([i,j]);
+            }
+        }
+    }
+    return pairsIndex;
+};
+
+Array.prototype.transpose = function () {
+    let transposed = [];
+
+    for (let i = 0; i < this.length; i++){
+        let row = [];
+        for(let j = 0; j < this.length; j++){
+            row.push(this[j][i]);
+        }
+        transposed.push(row);
+    }
+    return transposed;
+};
+
